6fa924e3b8b21a0c990d257506de312bdf51a797
matviets.m <matviets.m@corp.click.uz>
2025-12-19 16:07:31 +0500

    small adjustments. user response now contains all user data.

--- src/main/java/com/example/gsp/controller/UserController.java
+++ src/main/java/com/example/gsp/controller/UserController.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/gcp/users")
+@RequestMapping("/gcp/users")
 public class UserController {
 
     private final UserServiceImpl userService;
--- src/main/java/com/example/gsp/dto/user/CreateUserResponseDto.java
+++ src/main/java/com/example/gsp/dto/user/CreateUserResponseDto.java
@@ -1,7 +1,22 @@
 package com.example.gsp.dto.user;
 
+import com.example.gsp.constant.enums.Gender;
+import com.example.gsp.constant.enums.IdentificationDocument;
+
+import java.time.LocalDate;
+
 public record CreateUserResponseDto(
     Long id,
     String name,
-    String pinfl
+    String address,
+    String phoneNumber,
+    String email,
+    String photoUrl,
+    String pinfl,
+    Integer age,
+    Gender gender,
+    IdentificationDocument documentType,
+    LocalDate issueDate,
+    LocalDate expiryDate,
+    String citizenship
 ) {}
